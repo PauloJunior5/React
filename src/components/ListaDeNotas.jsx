3a6c5748e7ec97f4e7542db56f3e94c9d423667b
Paulo Junior <pjunior.live@gmail.com>
2021-01-06 14:45:15 -0300

    Styling components

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import CardDeNotas from "./CardDeNotas";
+import CardDeNotas from "./cardDeNotas/CardDeNotas";
 
 class ListaDeNotas extends Component {
     render() {
         return (
             <ul>
-                {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+                {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
                     return(
-                        <li>
+                        <li key={index}>
                             <div>{categoria}</div>
                             <CardDeNotas />
                         </li>
