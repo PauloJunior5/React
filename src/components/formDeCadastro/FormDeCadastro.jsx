d4615e9c8987166ec6dcf61666f250e3bdaeba98
Paulo Junior <pjunior.live@gmail.com>
2021-01-07 02:12:10 -0300

    dealing with events and binds

--- src/components/formDeCadastro/FormDeCadastro.jsx
+++ src/components/formDeCadastro/FormDeCadastro.jsx
@@ -2,10 +2,20 @@ import React, { Component } from "react";
 import './estilo.css';
 
 class FormDeCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = '';
+  }
+
+  handlerMudancaTitulo(evento) {
+    this.titulo = evento.target.value;
+    console.log(this.titulo);
+  }
+
   render() {
     return (
       <form className='form-cadastro'>
-        <input type="text" placeholder="Titulo" />
+        <input type="text" placeholder="Titulo" onChange={this.handlerMudancaTitulo.bind(this)}/>
         <textarea placeholder="Escreva sua nota..." rows={15} />
         <button className='form-cadastro_input form-cadastro_submit'>Criar Nota</button>
       </form>
